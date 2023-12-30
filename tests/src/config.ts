b3552dd9e392ae0dddf45ed38c34aad8210e1675
Nabarun Sarkar <nabsarkar@gmail.com>
2023-12-30 16:01:22 +0400

    Cleanup

--- tests/src/config.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  WalletMnemonic:
-    "banner spread envelope side kite person disagree path silver will brother under couch edit food venture squirrel civil budget number acquire point work mass",
-  HostWallet: "osmo1m9l358xunhhwds0568za49mzhvuxx9uxtz8m2l",
-  IcaAddress: "osmo1erl4adm3d5klfn8sdfwqg3jf3tyzf7epsu6d5h3px0z8ng72245svsljr4",
-};
