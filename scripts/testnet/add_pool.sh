3091a6f67e8d72b4898e165c6144762788f60178
Nabarun Sarkar <nabsarkar@gmail.com>
2024-01-28 09:15:58 +0530

    Update add_pool.sh

--- scripts/testnet/add_pool.sh
+++ scripts/testnet/add_pool.sh
@@ -12,9 +12,9 @@ msg=$(cat << EOF
 {
   "add_pool": {
     "pool_id": 356,
-    "default_bond_denom": "ibc/47847622DF5F34239E71986A819598F8F3727FE2DA021D99058DC31A2C5F364E",
-    "stk_token_denom": "ibc/9FF2B7A5F55038A7EE61F4FD6749D9A648B48E89830F2682B67B5DC158E2753C",
-    "asset_ordering": "native_token_first"
+    "default_bond_denom": "uatom",
+    "stk_token_denom": "stk/uatom",
+    "asset_ordering": "stk_token_first"
   }
 }
 EOF
